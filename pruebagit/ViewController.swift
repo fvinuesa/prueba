f0d7538396cd8a13d61354fa58473b0d3640e969
Francisco Vinuesa Caro <fvinuesa@quartanatura.com>
2015-12-06 20:10:19 +0100

    Initial Commit

---
+++ pruebagit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebagit
+//
+//  Created by Francisco Vinuesa Caro on 6/12/15.
+//  Copyright © 2015 Francisco Vinuesa Caro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
